48c11d8e58c80730f77593ba9bbaa88c6146ffec
GrahSol <anugrahpalesangpasinggi@gmail.com>
2026-07-19 21:46:01 +0700

    fix: use DATABASE_URL environment variable in prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -10,6 +10,6 @@ export default defineConfig({
     seed: "node prisma/seed.js",
   },
   datasource: {
-    url: process.env["mysql://root:@127.0.0.1:3306/tubesCheva_db"],
+    url: process.env["DATABASE_URL"],
   },
 });
